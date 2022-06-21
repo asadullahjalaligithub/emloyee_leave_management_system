b594b4391fd6e8b074748218e0d5e1554247d5d5
asadullahjalaligithub <asadullah.jalali2013@gmail.com>
2022-06-21 18:03:36 +0430

    adding files

---
+++ myprofile.php
@@ -0,0 +1,314 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/config.php');
+if(strlen($_SESSION['emplogin'])==0)
+    {   
+header('location:index.php');
+}
+else
+{
+  
+   // exit();
+    $eid=$_SESSION['emplogin'];
+    if(isset($_POST['update']))
+    {
+        $generalError="";
+        // firstname
+        if(empty($_POST['firstName']))
+        {
+            $generalError.="* firstName is empty";
+        }
+        if(is_numeric($_POST['firstName']))
+        {
+            $generalError.="* firstName is numeric";
+        }
+        // lastname 
+        if(empty($_POST['lastName']))
+        {
+            $generalError.="* lastName is empty";
+        }
+        if(is_numeric($_POST['lastName']))
+        {
+            $generalError.="* lastName is numeric";
+        }
+        // mobile number
+        if(!is_numeric(substr($_POST['mobileno'],1)))
+        {
+            $generalError.="* mobileno is not numeric";
+        }
+        if(empty($_POST['mobileno']))
+        {
+            $generalError.="* mobile is empty";
+        }
+    
+        // date of birth
+        if(empty($_POST['dob']))
+        {
+            $generalError.="* mobile is empty";
+        }
+        // date of birth
+        if(empty($_POST['department']))
+        {
+            $generalError.="* department is empty";
+        }
+        // date of birth
+        if(empty($_POST['city']))
+        {
+            $generalError.="* city is empty";
+        }
+        // date of birth
+        if(empty($_POST['address']))
+        {
+            $generalError.="* address is empty";
+        }
+        
+        if(empty($generalError)) {
+    $fname=$_POST['firstName'];
+    $lname=$_POST['lastName'];   
+    $gender=$_POST['gender']; 
+    $dob=$_POST['dob']; 
+    $department=$_POST['department']; 
+    $address=$_POST['address']; 
+    $city=$_POST['city']; 
+    $country=$_POST['country']; 
+    $mobileno=$_POST['mobileno']; 
+    $sql="update tblemployees set FirstName=:fname,LastName=:lname,Gender=:gender,Dob=:dob,Department=:department,Address=:address,City=:city,Country=:country,Phonenumber=:mobileno where EmailId=:eid";
+    $query = $dbh->prepare($sql);
+    $query->bindParam(':fname',$fname,PDO::PARAM_STR);
+    $query->bindParam(':lname',$lname,PDO::PARAM_STR);
+    $query->bindParam(':gender',$gender,PDO::PARAM_STR);
+    $query->bindParam(':dob',$dob,PDO::PARAM_STR);
+    $query->bindParam(':department',$department,PDO::PARAM_STR);
+    $query->bindParam(':address',$address,PDO::PARAM_STR);
+    $query->bindParam(':city',$city,PDO::PARAM_STR);
+    $query->bindParam(':country',$country,PDO::PARAM_STR);
+    $query->bindParam(':mobileno',$mobileno,PDO::PARAM_STR);
+    $query->bindParam(':eid',$eid,PDO::PARAM_STR);
+    $query->execute();
+    $msg="Employee record updated Successfully";
+    }
+
+    }
+    ?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        
+        <!-- Title -->
+        <title>Admin | Update Employee</title>
+        
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+        <meta charset="UTF-8">
+        <meta name="description" content="Responsive Admin Dashboard Template" />
+        <meta name="keywords" content="admin,dashboard" />
+        <meta name="author" content="Steelcoders" />
+        
+        <!-- Styles -->
+        <link type="text/css" rel="stylesheet" href="assets/plugins/materialize/css/materialize.min.css"/>
+        <link type="text/css" rel="stylesheet" href="assets/plugins/materialize/css/materialize.css"/>
+        <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link href="assets/plugins/material-preloader/css/materialPreloader.min.css" rel="stylesheet"> 
+        <link href="assets/css/alpha.min.css" rel="stylesheet" type="text/css"/>
+        <link href="assets/css/custom.css" rel="stylesheet" type="text/css"/>
+  <style>
+        .errorWrap {
+    padding: 10px;
+    margin: 0 0 20px 0;
+    background: #fff;
+    color:red;
+    border-left: 4px solid #dd3d36;
+    -webkit-box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
+    box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
+}
+.succWrap{
+    padding: 10px;
+    margin: 0 0 20px 0;
+    background: #fff;
+    color:green;
+    border-left: 4px solid #5cb85c;
+    -webkit-box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
+    box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
+}
+        </style>
+
+
+
+
+
+    </head>
+    <body>
+  <?php include('includes/header.php');?>
+            
+       <?php include('includes/sidebar.php');?>
+   <main class="mn-inner">
+                <div class="row">
+                    <div class="col s12">
+                        <div class="page-title" style="color: green;">Update employee details</div>
+                    </div>
+                    <div class="col s12 m12 l12">
+                        <div class="card">
+                            <div class="card-content">
+                                <form id="example-form" method="post" name="updatemp">
+                                    <div><?php 
+                                     
+                                
+                                      
+                                    ?>
+                                        
+                                           <?php if($generalError){?>
+                                           <div class="errorWrap"><strong>ERROR</strong>:<?php echo htmlentities($generalError); ?> </div>
+                                           <?php } 
+                                            if($emptyError){?>
+                                            <div class="errorWrap"><strong>ERROR</strong>:<?php echo htmlentities($emptyError); ?> </div>
+
+                                            <?php }
+                                             if($firstnameError){?>
+                                                <div class="errorWrap"><strong>ERROR</strong>:<?php echo htmlentities($firstnameError); ?> </div>
+                                                <?php } 
+                                                 if($lastnameError){?>
+                                                    <div class="errorWrap"><strong>ERROR</strong>:<?php echo htmlentities($lastnameError); ?> </div>
+                                                    <?php } 
+                                                      if($mobilenoError){?>
+                                                        <div class="errorWrap"><strong>ERROR</strong>:<?php echo htmlentities($mobilenoError); ?> </div>
+                                                        <?php } 
+                else if($msg){?><div class="succWrap"><strong>SUCCESS</strong> : <?php echo htmlentities($msg); ?> </div><?php }?>
+                                        <section>
+                                            <div class="wizard-content">
+                                                <div class="row">
+                                                    <div class="col m6">
+                                                        <div class="row">
+<?php 
+$eid=$_SESSION['emplogin'];
+$sql = "SELECT * from  tblemployees where EmailId=:eid";
+$query = $dbh -> prepare($sql);
+$query -> bindParam(':eid',$eid, PDO::PARAM_STR);
+$query->execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+$cnt=1;
+if($query->rowCount() > 0)
+{
+foreach($results as $result)
+{               ?> 
+ <div class="input-field col  s12">
+<label for="empcode">Employee Code</label>
+<input  name="empcode" id="empcode" value="<?php echo htmlentities($result->EmpId);?>" type="text" autocomplete="off" readonly >
+<span id="empid-availability" style="font-size:12px;"></span> 
+</div>
+
+
+<div class="input-field col m6 s12">
+<label for="firstName">First name</label>
+<input id="firstName" name="firstName" value="<?php echo htmlentities($result->FirstName);?>"  type="text" >
+</div>
+
+<div class="input-field col m6 s12">
+<label for="lastName">Last name </label>
+<input id="lastName" name="lastName" value="<?php echo htmlentities($result->LastName);?>" type="text" autocomplete="off" >
+</div>
+
+<div class="input-field col s12">
+<label for="email">Email</label>
+<input  name="email" type="email" id="email" value="<?php echo htmlentities($result->EmailId);?>" readonly autocomplete="off" required>
+<span id="emailid-availability" style="font-size:12px;"></span> 
+</div>
+
+<div class="input-field col s12">
+<label for="phone">Mobile number</label>
+<input id="phone" name="mobileno" type="tel" value="<?php echo htmlentities($result->Phonenumber);?>" maxlength="10" autocomplete="off" required>
+ </div>
+
+</div>
+</div>
+                                                    
+<div class="col m6"> 
+
+<div class="row">
+
+<div class="input-field col m6 s12">
+
+<select  name="gender" autocomplete="off">
+<option value="<?php echo htmlentities($result->Gender);?>"><?php echo htmlentities($result->Gender);?></option>                                          
+<option value="Male">Male</option>
+<option value="Female">Female</option>
+<option value="Other">Other</option>
+</select>
+</div>
+<label for="birthdate">Date of Birth</label>
+<div class="input-field col m6 s12">
+
+<input id="birthdate" name="dob"  class="datepicker" value="<?php echo htmlentities($result->Dob);?>" >
+</div>
+
+                                                    
+
+<div class="input-field col m6 s12">
+<select  name="department" autocomplete="off">
+<option value="<?php echo htmlentities($result->Department);?>"><?php echo htmlentities($result->Department);?></option>
+<?php $sql = "SELECT DepartmentName from tbldepartments";
+$query = $dbh -> prepare($sql);
+$query->execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+$cnt=1;
+if($query->rowCount() > 0)
+{
+foreach($results as $resultt)
+{   ?>                                            
+<option value="<?php echo htmlentities($resultt->DepartmentName);?>"><?php echo htmlentities($resultt->DepartmentName);?></option>
+<?php }} ?>
+</select>
+</div>
+
+
+   
+<div class="input-field col m6 s12">
+<label for="country">Country</label>
+<input id="country" name="country" type="text"  value="<?php echo htmlentities($result->Country);?>" autocomplete="off" >
+</div>
+
+<div class="input-field col m6 s12">
+<label for="city">City/Town</label>
+<input id="city" name="city" type="text"  value="<?php echo htmlentities($result->City);?>" autocomplete="off" >
+ </div>                                                          
+
+<div class="input-field col m6 s12">
+<label for="address">Address</label>
+<input id="address" name="address" type="text"  value="<?php echo htmlentities($result->Address);?>" autocomplete="off" >
+</div>
+<?php }}?>
+                                                        
+<div class="input-field col s12">
+<button type="submit" name="update"  id="update" class="waves-effect waves-light btn indigo m-b-xs">UPDATE</button>
+
+</div>
+
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </section>
+                                     
+                                    
+                                        </section>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </div>
+        <div class="left-sidebar-hover"></div>
+        
+        <!-- Javascripts -->
+        <script src="assets/plugins/jquery/jquery-2.2.0.min.js"></script>
+        <script src="assets/plugins/materialize/js/materialize.min.js"></script>
+        <script src="assets/plugins/material-preloader/js/materialPreloader.min.js"></script>
+        <script src="assets/plugins/jquery-blockui/jquery.blockui.js"></script>
+        <script src="assets/js/alpha.min.js"></script>
+        <script src="assets/js/pages/form_elements.js"></script>
+        
+    </body>
+</html>
+<?php } ?> 
